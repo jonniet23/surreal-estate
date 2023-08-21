0a7535d31e7fccae1f4808a482460b7b72dbdbc6
Jonnie <jonniet23@hotmail.com>
2023-08-21 17:35:37 +0100

    first commit initialising project

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import "../styles/app.css";
+
+function App() {
+  return (
+    <div className="App">
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <h2>Surreal Estate</h2> Learn React
+      </a>
+    </div>
+  );
+}
+
+export default App;
